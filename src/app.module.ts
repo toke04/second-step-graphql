75400ad1d7456951f29704dc4c0d7ad9350f3f1a
toke <54713809+syo-tokeshi@users.noreply.github.com>
2024-08-31 12:42:56 +0900

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import * as path from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: path.join(process.cwd(), "src/schema.gql"),
+      sortSchema: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
